eeff4bba2919ab7b7c679265384dee95da6c2da3
Winston Cooke <git@winstoncooke.com>
2024-04-23 12:19:39 -0500

    Add methods to insert, remove, and retrieve cards. Also add method to print as string.

--- src/hand.rs
+++ src/hand.rs
@@ -6,7 +6,42 @@ pub struct Hand {
 }
 
 impl Hand {
-    pub fn print_symbols(&self) {
+    pub fn new() -> Self {
+        let cards = Vec::new();
+
+        Self { cards }
+    }
+
+    /// Returns the cards in the Hand.
+    pub fn get_cards(&self) -> &Vec<Card> {
+        &self.cards
+    }
+
+    /// Appends a Card to the back of the Hand.
+    pub fn push(&mut self, card: Card) {
+        self.cards.push(card);
+    }
+
+    /// Returns the Card at the back of the Hand if any.
+    pub fn pop(&mut self) -> Option<Card> {
+        if let Some(card) = self.cards.pop() {
+            return Some(card);
+        }
+
+        None
+    }
+
+    pub fn to_string(&self) {
+        let mut string_to_print = String::new();
+
+        for &card in self.cards.iter() {
+            string_to_print.push_str(&card.to_string());
+        }
+
+        println!("{}", string_to_print);
+    }
+
+    pub fn to_symbols(&self) {
         let mut string_to_print = String::new();
 
         for (i, &card) in self.cards.iter().enumerate() {
